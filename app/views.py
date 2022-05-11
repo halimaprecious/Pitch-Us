75796fa22dbeba12c7517c511896560086d7b809
halima precious <halimaprecious3@gmail.com>
2022-05-11 10:33:34 +0300

    ft:user profile route

--- app/views.py
+++ app/views.py
@@ -38,42 +38,42 @@ views = Blueprint("views",__name__)
 #    return render_template('pitch-us.html',user=current_user,form =form)
 
 #user profile
-# @views.route('/user/<username>')
-# def profile(username):
-#    user = User.query.filter_by(username=username).first()
-#    if not user:
-#       flash("User does not exist!",category='error')
+@views.route('/user/<username>')
+def profile(username):
+   user = User.query.filter_by(username=username).first()
+   if not user:
+      flash("User does not exist!",category='error')
 
-#    return render_template("profile/profile.html", user = user)
+   return render_template("profile/profile.html", user = user)
 
 #update profile
-# @views.route('/user/<username>/update',methods =['GET','POST'])
-# @login_required
-# def update_profile(username):
-#    user = User.query.filter_by(username = username).first()
-#    if not user:
-#       flash("User does not exist!",category='error')
+@views.route('/user/<username>/update',methods =['GET','POST'])
+@login_required
+def update_profile(username):
+   user = User.query.filter_by(username = username).first()
+   if not user:
+      flash("User does not exist!",category='error')
 
-#    form = UpdateProfile()
-#    if form.validate_on_submit():
-#       user.bio = form.bio.data
-#       db.session.add(user)
-#       db.session.commit()
-#       return redirect(url_for('.profile',username=user.username))
+   form = UpdateProfile()
+   if form.validate_on_submit():
+      user.bio = form.bio.data
+      db.session.add(user)
+      db.session.commit()
+      return redirect(url_for('.profile',username=user.username))
 
-#    return render_template('profile/update.html',form =form)
+   return render_template('profile/update.html',form =form)
 
 #profile pic
-# @views.route('/user/<username>/update/pic',methods=['POST'])
-# @login_required
-# def update_pic(username):
-#    user = User.query.filter_by(username=username).first()
-#    if 'photo' in request.files:
-#       filename = photos.save(request.files['photo'])
-#       path = f'photos/{filename}'
-#       user.profile_pic_path = path
-#       db.session.commit()
-#    return redirect(url_for('views.profile',username=username))
+@views.route('/user/<username>/update/pic',methods=['POST'])
+@login_required
+def update_pic(username):
+   user = User.query.filter_by(username=username).first()
+   if 'photo' in request.files:
+      filename = photos.save(request.files['photo'])
+      path = f'photos/{filename}'
+      user.profile_pic_path = path
+      db.session.commit()
+   return redirect(url_for('views.profile',username=username))
 
 # User's pitch
 # @views.route('/pitch/<username>')
