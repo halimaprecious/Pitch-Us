b65240d41653b3818882fe137f2fac8252a1c800
halima precious <halimaprecious3@gmail.com>
2022-05-11 10:24:12 +0300

    adds registration routes and authentication

--- app/auth.py
+++ app/auth.py
@@ -10,60 +10,60 @@ from werkzeug.security import generate_password_hash, check_password_hash
 auth = Blueprint("auth",__name__)
 
 
-# @auth.route('/login',methods = ['GET','POST'])
-# def login(): 
-#    if request.method == 'POST':
-#       email = request.form.get("email")
-#       password = request.form.get("password")
+@auth.route('/login',methods = ['GET','POST'])
+def login(): 
+   if request.method == 'POST':
+      email = request.form.get("email")
+      password = request.form.get("password")
 
-#       user = User.query.filter_by(email=email).first()
-#       if user:
-#          if check_password_hash(user.password,password):
-#             flash("Logged In!",category = 'success')
-#             login_user(user,remember=True)
-#             return redirect(url_for('views.home'))
-#          else:
-#             flash("Password is incorrect",category='error')
-#       elif not email or not password:
-#          flash("Please enter email and password",category='error')
-#       else:
-#          flash('Email does not exist',category = 'error')
+      user = User.query.filter_by(email=email).first()
+      if user:
+         if check_password_hash(user.password,password):
+            flash("Logged In!",category = 'success')
+            login_user(user,remember=True)
+            return redirect(url_for('views.home'))
+         else:
+            flash("Password is incorrect",category='error')
+      elif not email or not password:
+         flash("Please enter email and password",category='error')
+      else:
+         flash('Email does not exist',category = 'error')
          
-#    return render_template('auth/login.html',user = current_user)
+   return render_template('auth/login.html',user = current_user)
 
-# @auth.route('/sign-up',methods = ['GET','POST'])
-# def signup():
-#    if request.method == 'POST':
-#       email= request.form.get("email")
-#       username = request.form.get("username")
-#       password1 = request.form.get("password1")
-#       password2 = request.form.get("password2")
+@auth.route('/sign-up',methods = ['GET','POST'])
+def signup():
+   if request.method == 'POST':
+      email= request.form.get("email")
+      username = request.form.get("username")
+      password1 = request.form.get("password1")
+      password2 = request.form.get("password2")
       
-#       email_exists = User.query.filter_by(email=email).first()
-#       username_exists = User.query.filter_by(username=username).first()
-#       if email_exists:
-#          flash('Email is already in use.',category='error')
-#       elif username_exists:
-#          flash('Username is already in use.',category='error')
-#       elif password1 != password2:
-#          flash('Passsword didn\'t match!.',category='error')
-#       elif not email or not username and not password1:
-#          flash('Please fill in all fields!',category = 'error')
-#       else:
-#          new_user = User(email=email,username=username,password=generate_password_hash(password1,method='sha256'))
-#          db.session.add(new_user)
-#          db.session.commit()
+      email_exists = User.query.filter_by(email=email).first()
+      username_exists = User.query.filter_by(username=username).first()
+      if email_exists:
+         flash('Email is already in use.',category='error')
+      elif username_exists:
+         flash('Username is already in use.',category='error')
+      elif password1 != password2:
+         flash('Passsword didn\'t match!.',category='error')
+      elif not email or not username and not password1:
+         flash('Please fill in all fields!',category = 'error')
+      else:
+         new_user = User(email=email,username=username,password=generate_password_hash(password1,method='sha256'))
+         db.session.add(new_user)
+         db.session.commit()
 
-         # mail_message("Welcome to PitchUs","email/welcome_user",new_user.email,user=new_user)
+         mail_message("Welcome to PitchUs","email/welcome_user",new_user.email,user=new_user)
 
-   #       login_user(new_user,remember=True)
-   #       flash('User created!')
-   #       return redirect(url_for('auth.login'))
+         login_user(new_user,remember=True)
+         flash('User created!')
+         return redirect(url_for('auth.login'))
   
-   # return render_template('auth/sign-up.html',user = current_user)
+   return render_template('auth/sign-up.html',user = current_user)
 
-# @auth.route('/logout')
-# @login_required
-# def logout():
-#    logout_user()
-#    return redirect(url_for('views.home'))
+@auth.route('/logout')
+@login_required
+def logout():
+   logout_user()
+   return redirect(url_for('views.home'))
