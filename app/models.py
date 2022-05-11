56ce3305a9b38a62ead3ea9e1663555ba70d0b60
halima precious <halimaprecious3@gmail.com>
2022-05-11 10:51:33 +0300

    ft:add user model

--- app/models.py
+++ app/models.py
@@ -1,19 +1,19 @@
-# from . import db
-# from flask_login import UserMixin
-# from sqlalchemy.sql import func
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 
-# class User(db.Model,UserMixin):
-#     id = db.Column(db.Integer,primary_key = True)
-#     email = db.Column(db.String(150),unique = True)
-#     username = db.Column(db.String(150),unique = True)
-#     password = db.Column(db.String(150),nullable = False)
-#     bio = db.Column(db.String(255))
-#     profile_pic_path = db.Column(db.String())
-#     date_created = db.Column(db.DateTime(timezone=True),default=func.now())
-#     pitches = db.relationship('Pitch',backref='user',passive_deletes=True)
-#     comments = db.relationship('Comment',backref='user',passive_deletes=True)
-#     likes = db.relationship('Like',backref='user',passive_deletes=True)
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer,primary_key = True)
+    email = db.Column(db.String(150),unique = True)
+    username = db.Column(db.String(150),unique = True)
+    password = db.Column(db.String(150),nullable = False)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    date_created = db.Column(db.DateTime(timezone=True),default=func.now())
+    pitches = db.relationship('Pitch',backref='user',passive_deletes=True)
+    comments = db.relationship('Comment',backref='user',passive_deletes=True)
+    likes = db.relationship('Like',backref='user',passive_deletes=True)
 
 # class Pitch(db.Model):
 #     id = db.Column(db.Integer,primary_key = True)
